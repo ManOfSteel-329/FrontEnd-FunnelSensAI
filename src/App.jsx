e1fde4e5b89541ecac79df038e8165d440644c2c
SNHUPhoebe <phoebe.johnson2@snhu.edu>
2024-12-19 15:26:28 -0600

    Toggle Button for Light and Dark Mode.
    Simple background and text colors are toggled and may be adjusted in App.css. I also flipped the icons for light and dark mode because I thought it made more sense to have them the other way, but I can flip them around if necessary.

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,3 @@
-<<<<<<< Updated upstream
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Navbar from './Navbar'
-// import './App.css'
-
-function App() {
-//   const [count, setCount] = useState(0)
-
-  return (
-
-    <div>
-        <Navbar />
-        <h1 className="text-blue-700">Hello</h1>
-        <p className="text-red-700">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis, natus delectus repellat aliquid fuga ipsum sunt amet totam excepturi numquam.</p>
-    </div>
-
-  )
-}
-
-export default App
-=======
 import React from 'react'
 import ThemeToggle from './components/ThemeToggle'
 import './App.css'
@@ -33,4 +10,3 @@ export default function App() {
         </>
     )
 }
->>>>>>> Stashed changes
