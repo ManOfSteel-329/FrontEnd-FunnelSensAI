9adbabeafc8a6b357712f572a3dc998bb95d112a
SNHUPhoebe <165221809+s0ftwareDeveloper@users.noreply.github.com>
2024-12-29 11:35:06 -0600

    Updated toggle code

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,27 +1,28 @@
-import React, { useState, useEffect } from 'react';
-import { FaSun, FaMoon } from 'react-icons/fa';
+import React, { useState } from 'react';
 import "./ThemeToggle.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Brightness7, DarkMode } from "@mui/icons-material";
 
 export default function ThemeToggle() {
 
-const [theme, setTheme] = useState('light'); // default theme is light
+    const [theme, setTheme] = useState('light');
 
-useEffect(() => {
-    document.body.className = theme;
-}, [theme])
-
-function toggleTheme() {
-    setTheme(theme === 'light' ? 'dark' : 'light');
-}
+    const toggleTheme = () => {
+        setTheme(theme === 'light' ? 'dark' : 'light');
+        document.body.className = theme;
+    };
 
     return (
-        <div className="toggle-container">
-        <button onClick={toggleTheme} className={`toggle-button ${theme}`}>
-            <span className={`toggle-icon ${theme}`}>
-                {theme === 'light' ? <FaMoon /> : <FaSun />}
-            </span>
-        </button>
-        </div>
-    )
+            <div
+                className={`toggle-track ${theme}`}
+                onClick={toggleTheme}
+            >
+                <div
+                    className={`toggle-circle ${theme}`}
+                >
+                    {theme === 'dark' ? <DarkMode className={`${theme}`} /> : <Brightness7 className={`${theme}`} />}
+                </div>
+            </div>
+    );
 }
 
